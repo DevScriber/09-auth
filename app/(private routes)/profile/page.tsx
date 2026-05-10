77989213046653d0feb8528173906551567b0622
Eugene Mukhin <eug.muhin@gmail.com>
2026-05-10 05:15:53 +0300

    Refactor ProfilePage

--- app/(private routes)/profile/page.tsx
+++ app/(private routes)/profile/page.tsx
@@ -23,32 +23,42 @@ export const metadata: Metadata = {
     },
 };
 
+
 const ProfilePage = async () => {
+
+
     const user = await getMeServer();
-    return (
-        <main className={css.mainContent}>
-            <div className={css.profileCard}>
-                <div className={css.header}>
-                    <h1 className={css.formTitle}>Profile Page</h1>
-                    <Link href="/profile/edit" className={css.editProfileButton}>
-                        Edit Profile
-                    </Link>
+
+    if (!user) {
+
+        return null;
+    }
+    else {
+        return (
+            <main className={css.mainContent}>
+                <div className={css.profileCard}>
+                    <div className={css.header}>
+                        <h1 className={css.formTitle}>Profile Page</h1>
+                        <Link href="/profile/edit" className={css.editProfileButton}>
+                            Edit Profile
+                        </Link>
+                    </div>
+                    <div className={css.avatarWrapper}>
+                        <Image
+                            src={user?.avatar}
+                            alt="User Avatar"
+                            width={120}
+                            height={120}
+                            className={css.avatar}
+                        />
+                    </div>
+                    <div className={css.profileInfo}>
+                        <p>Username: {user?.username}</p>
+                        <p>Email: {user?.email}</p>
+                    </div>
                 </div>
-                <div className={css.avatarWrapper}>
-                    <Image
-                        src={user?.avatar}
-                        alt="User Avatar"
-                        width={120}
-                        height={120}
-                        className={css.avatar}
-                    />
-                </div>
-                <div className={css.profileInfo}>
-                    <p>Username: {user?.username}</p>
-                    <p>Email: {user?.email}</p>
-                </div>
-            </div>
-        </main>
-    );
-};
+            </main>
+        );
+    };
+}
 export default ProfilePage;
